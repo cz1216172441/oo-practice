4cdcbe3f6af9893cbe2f314c507628779659f529
CHENRO6 <chenro6@oocl.com>
2020-07-22 18:06:54 +0800

    [OO Step by Step][Roy]feature: completed and passed Practice06Test

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = -1;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == -1) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass);
+    }
 }
