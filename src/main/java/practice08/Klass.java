f1adcbc668b07c30b1b58fbe66348e29c78f7715
CHENRO6 <chenro6@oocl.com>
2020-07-22 19:44:24 +0800

    [OO Step by Step][Roy]feature: completed and passed Practice08Test

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,29 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+    private String displayName;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class " + this.number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
